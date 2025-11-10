ed6e2a3a2fb565d18bbc7ec54f37b8ca65c3cd55
Smeltier <connect.goncalves@gmail.com>
2025-11-10 12:36:59 -0300

    add: adiciona a entidade base e as pastas default

---
+++ src/entities/base_game_entity.py
@@ -0,0 +1,20 @@
+import pygame
+
+from abc import abstractmethod
+
+class BaseGameEntity ():
+    _next_ID = 1000
+
+    def __init__(self, x, y, world):
+        self.ID = self._set_ID()
+        self.position = pygame.Vector2((x, y))
+        self.environment = world
+
+    def _set_ID(self) -> int:
+        entity_ID = BaseGameEntity._next_ID
+        BaseGameEntity._next_ID += 1
+        return entity_ID
+    
+    @abstractmethod
+    def update(self, delta_time) -> None:
+        pass
