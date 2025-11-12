0621d68fcbe8a2747f56274c264e4532253acff4
Smeltier <connect.goncalves@gmail.com>
2025-11-12 09:21:53 -0300

    add: adiciona MultiTargetSteering

---
+++ src/states/multi_target_steering.py
@@ -0,0 +1,33 @@
+import pygame
+
+from abc import abstractmethod
+
+from src.states.state import State
+from src.outputs.steering_output import SteeringOutput
+
+class MultiTargetSteering (State):
+
+    def __init__(self, entity, threshold=100):
+        super().__init__(entity)
+
+        if threshold <= 0:
+            raise ValueError('threshold deve ser maior que zero.')
+
+        self.targets = self.entity.environment.entities
+        self.threshold = threshold
+
+    @abstractmethod
+    def enter(self):
+        pass
+
+    @abstractmethod
+    def exit(self):
+        pass
+    
+    abstractmethod
+    def execute(self, delta_time):
+        pass
+    
+    @abstractmethod
+    def get_steering(self):
+        pass
