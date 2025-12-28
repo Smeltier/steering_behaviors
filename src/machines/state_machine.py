6fc6d1e2216dda6b5505094eb5b0b7b00e35fa70
Smeltier <connect.goncalves@gmail.com>
2025-12-28 18:57:58 -0300

    att: atualiza atributos na classe StateMachine e resolve importe circular.

--- src/machines/state_machine.py
+++ src/machines/state_machine.py
@@ -1,47 +1,61 @@
-class StateMachine ():
+from typing import TYPE_CHECKING
 
-    def __init__(self, owner, start_state=None):
+if TYPE_CHECKING:
+    from src.states.state import State
+    from src.entities.moving_entity import MovingEntity
+
+class StateMachine:
+
+    _owner: 'MovingEntity'
+    _current_state: 'State'
+    _previous_state: 'State'
+
+    def __init__(self, owner: 'MovingEntity', start_state: 'State' = None) -> None:
         if not owner:
             raise ValueError("O 'owner' da StateMachine não pode ser None.")
 
-        self.owner          = owner
-        self.current_state  = start_state
-        self.previous_state = None
+        self._owner = owner
+        self._current_state = start_state
+        self._previous_state = None
 
-        if self.current_state:
-            self.current_state.enter()
+        if self._current_state:
+            self._current_state.enter()
 
-    def update(self, delta_time):
+    def update(self, delta_time: float) -> None:
         """ Atualiza a Máquina de Estados. """
 
-        if not self.current_state: return
+        if not self._current_state: 
+            return
 
         try:
-            self.current_state.execute(delta_time)
+            self._current_state.execute(delta_time)
         except Exception as e:
-            print(f"[ERROR] Falha no {self.current_state}.execute(): {e}")
-            self.current_state = None
+            print(f"[ERROR] Falha no {self._current_state}.execute(): {e}")
+            self._current_state = None
     
-    def change_state(self, new_state):
+    def change_state(self, new_state: 'State') -> None:
         """ Troca o estado atual. """
 
-        if not new_state: return
+        if not new_state: 
+            return
 
-        if self.current_state:
-            self.current_state.exit()
+        if self._current_state:
+            self._current_state.exit()
         
-        self.previous_state = self.current_state
-        self.current_state = new_state
+        self._previous_state = self._current_state
+        self._current_state = new_state
         
         try:
-            self.current_state.enter()
+            self._current_state.enter()
             
         except Exception as e:
-            print(f"[ERROR] Falha no {self.current_state}.enter(): {e}")
-            self.current_state = None
+            print(f"[ERROR] Falha no {self._current_state}.enter(): {e}")
+            self._current_state = None
 
-    def revert_to_previous_state(self):
+    def revert_to_previous_state(self) -> None:
         """ Troca para o estado anterior. """
 
-        if not self.previous_state: return
-        self.change_state(self.previous_state)        
+        if not self._previous_state: 
+            return
+        
+        self.change_state(self._previous_state)        
