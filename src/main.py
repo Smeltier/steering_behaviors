7e87a359d8f9cb275c75d35a72733d4bf055269c
Smeltier <connect.goncalves@gmail.com>
2025-12-28 19:00:07 -0300

    att:

--- src/main.py
+++ src/main.py
@@ -19,23 +19,23 @@ def main():
     world: World = World(screen)
 
     entity_one = MovingEntity(
-        x     = WIDTH  // 2,
-        y     = HEIGHT // 2,
+        x = WIDTH  // 2,
+        y = HEIGHT // 2,
         world = world, 
         max_speed = 300,
         max_acceleration = 500,
     )
 
     entity_two = MovingEntity(
-        WIDTH // 4, 
-        HEIGHT // 4, 
-        world, 
-        max_speed=300,
-        max_acceleration=500,
+        x = WIDTH // 4, 
+        y = HEIGHT // 4, 
+        world = world, 
+        max_speed = 300,
+        max_acceleration = 500,
     )
 
-    entity_one.state_machine.change_state(Arrive(entity_one, entity_two))
-    entity_two.state_machine.change_state(Wander(entity_two, MovingEntity(0, 0, world)))
+    entity_one.change_state(Arrive(entity_one, entity_two))
+    entity_two.change_state(Wander(entity_two, MovingEntity(0, 0, world)))
 
     world.add_entity(entity_one)
     world.add_entity(entity_two)
@@ -44,7 +44,7 @@ def main():
     while running:
         screen.fill('black')
 
-        delta_time = clock.tick(FPS) / 1000.0
+        delta_time: float = clock.tick(FPS) / 1000.0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
