6a94f7ae56b0168880178cd4bca10de2c3683b0d
Smeltier <connect.goncalves@gmail.com>
2026-04-29 19:47:38 -0300

    att: Modificando lógica de cálculo da normal de colisão.

--- simulations/deformacao_area_elipse.py
+++ simulations/deformacao_area_elipse.py
@@ -11,22 +11,30 @@ TILE_SIZE = 20
 FPS = 60
 DA, DB = 100, 100
 DR = 100
+STRENGTH_GAIN = 2.5
 
 def random_free_node(graph:GridGraph):
+    """ Escolhe um nó aleatório de graph.nodes. """
     return random.choice(graph.nodes)
 
 def draw_graph_lines(screen):
+    """ Desenha o grid que representa o Grafo """
     for x in range(0, WIDTH, TILE_SIZE):
         pg.draw.line(screen, (40, 40, 40), (x, 0), (x, HEIGHT))
     for y in range(0, HEIGHT, TILE_SIZE):
         pg.draw.line(screen, (40, 40, 40), (0, y), (WIDTH, y))
 
 def draw_obstacles(screen, graph:GridGraph):
+    """ Desenha os obstáculos presentes em graph.obstacles em uma superfície. """
     for obs in graph.obstacles:
         rect = (obs[0] * TILE_SIZE, obs[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE)
         pg.draw.rect(screen, "red", rect)
 
 def ellipse_collision_axes_rotated(center, a, b, theta, rect):
+    """ Verifica se a elipse colidiu com algum obstáculo e retorna o quanto 
+        ela entrou nele (em caso de colisão). 
+    """
+
     if a <= 0 or b <= 0:
         return False, 0.0, 0.0, 0.0
 
@@ -34,7 +42,7 @@ def ellipse_collision_axes_rotated(center, a, b, theta, rect):
     ct = math.cos(theta)
     st = math.sin(theta)
 
-    # G: desrotaciona ao redor do centro.
+    # G: Passa de coordenadas globais para um modelo local que é baseado na deformação da elipse.
     def to_local(px, py):
         dx = px - cx
         dy = py - cy
@@ -56,24 +64,28 @@ def ellipse_collision_axes_rotated(center, a, b, theta, rect):
     max_y = max(p[1] for p in lc)
 
     closest_x = 0.0
-    if 0.0 < min_x: 
+    if 0.0 < min_x:
         closest_x = min_x
-    elif 0.0 > max_x: 
+    elif 0.0 > max_x:
         closest_x = max_x
 
     closest_y = 0.0
-    if 0.0 < min_y: 
+    if 0.0 < min_y:
         closest_y = min_y
-    elif 0.0 > max_y: 
+    elif 0.0 > max_y:
         closest_y = max_y
 
     vx = closest_x / a
     vy = closest_y / b
     value = vx*vx + vy*vy
 
+    # G: Verifica se o ponto mais próximo encontrado está dentro da elipse.
+    #    Precisa ser <= 1 para estar contido na área da elipse.
     if value > 1.0:
         return False, 0.0, 0.0, 0.0
 
+    # G: Calcula o gradiente da elipse. Usaremos para calcular uma normal
+    #    que representa o quanto o bloco entrou na elipse.
     gx = closest_x / (a*a)
     gy = closest_y / (b*b)
 
@@ -84,19 +96,23 @@ def ellipse_collision_axes_rotated(center, a, b, theta, rect):
         db = abs(max_y - 0.0)
         m = min(dl, dr, dt, db)
 
-        if m == dl: 
+        if m == dl:
             gx, gy = -1.0, 0.0
-        elif m == dr: 
+        elif m == dr:
             gx, gy = 1.0, 0.0
-        elif m == dt: 
+        elif m == dt:
             gx, gy = 0.0, -1.0
-        else: 
+        else:
             gx, gy = 0.0, 1.0
 
+    # G: Normalização da normal.
     glen = math.hypot(gx, gy)
     nx = gx / glen
     ny = gy / glen
-    depth = 1.0 - value
+
+    # G: Cálculo da profundidade, ou seja, o quanto o obstáculo entrou na elipse.
+    r = math.sqrt(max(value, 1e-12))
+    depth = 1.0 - r
 
     return True, nx, ny, depth
 
@@ -110,25 +126,25 @@ def basic_deformation(ellipse:Ellipse, local_nx:float, local_ny:float, strength:
     ellipse.a = max(MIN_A, ellipse.a - kA * dt)
     ellipse.b = max(MIN_B, ellipse.b - kB * dt)
 
-def keep_area_deformation(ellipse:Ellipse, local_nx:float, local_ny:float, strength:float, dt:float) -> None:
-    K = ellipse.a * ellipse.b
-    if K <= 0:
-        return
-
-    wx = abs(local_nx)
-    wy = abs(local_ny)
-
-    da = DA * strength * wx * dt
-    db = DB * strength * wy * dt
-
-    new_a = max(MIN_A, ellipse.a - da)
-    new_b = max(MIN_B, ellipse.b - db)
-
-    new_K = new_a * new_b
-    if new_K > 0:
-        s = math.sqrt(K / new_K)
-        ellipse.a = min(MAX_A, max(MIN_A, new_a * s))
-        ellipse.b = min(MAX_B, max(MIN_B, new_b * s))
+# def keep_area_deformation(ellipse:Ellipse, local_nx:float, local_ny:float, strength:float, dt:float) -> None:
+#     K = ellipse.a * ellipse.b
+#     if K <= 0:
+#         return
+#
+#     wx = abs(local_nx)
+#     wy = abs(local_ny)
+#
+#     da = DA * strength * wx * dt
+#     db = DB * strength * wy * dt
+#
+#     new_a = max(MIN_A, ellipse.a - da)
+#     new_b = max(MIN_B, ellipse.b - db)
+#
+#     new_K = new_a * new_b
+#     if new_K > 0:
+#         s = math.sqrt(K / new_K)
+#         ellipse.a = min(MAX_A, max(MIN_A, new_a * s))
+#         ellipse.b = min(MAX_B, max(MIN_B, new_b * s))
 
 if __name__ == '__main__':
     pg.init()
@@ -160,31 +176,33 @@ if __name__ == '__main__':
 
         sum_nx = 0.0
         sum_ny = 0.0
-        sum_depth = 0.0
+        max_depth = 0.0
         contacts = 0
 
         for obs in graph.obstacles:
             rect = pg.Rect(obs[0] * TILE_SIZE, obs[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE)
             hit, nx, ny, depth = ellipse_collision_axes_rotated(ellipse_center, ellipse.a, ellipse.b, ellipse.rotation, rect)
+
             if hit:
                 sum_nx += nx
                 sum_ny += ny
-                sum_depth += depth
+                max_depth += max(max_depth, depth)
                 contacts += 1
 
         if contacts > 0:
             avg_nx = sum_nx / contacts
             avg_ny = sum_ny / contacts
             nlen = math.hypot(avg_nx, avg_ny)
+
             if nlen > 1e-6:
                 avg_nx /= nlen
                 avg_ny /= nlen
 
-            strength = min(1.0, sum_depth / contacts)
+            strength = min(1.0, max_depth * STRENGTH_GAIN)
             strength = strength ** 0.5
 
-            # basic_deformation(ellipse, avg_nx, avg_ny, strength, delta_time)
-            keep_area_deformation(ellipse, avg_nx, avg_ny, strength, delta_time)
+            basic_deformation(ellipse, avg_nx, avg_ny, strength, delta_time)
+            # keep_area_deformation(ellipse, avg_nx, avg_ny, strength, delta_time)
 
         else:
             ellipse.a = min(MAX_A, ellipse.a + DA * delta_time)
